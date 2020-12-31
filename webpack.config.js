7d87a496c694d54006b7e8c5671ce5dc3bdc2c2d
Aleksandr Bogoyavlensky <albg83@gmail.com>
2020-12-31 10:29:34 +0300

    scss plugin added

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require("copy-webpack-plugin");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
@@ -32,5 +33,17 @@ module.exports = {
         new MiniCssExtractPlugin({
             filename: 'bundle.[fullhash].css'
         })
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader"
+                ],
+            },
+        ],
+    }
 }
